62b6836ab7eee09d9df535131c38d5cf7c03307f
RiadMefti <riadmefti@hotmail.com>
2024-06-09 07:45:39 -0400

    creating db when serv starts

--- internal/database/database.go
+++ internal/database/database.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	_ "github.com/joho/godotenv/autoload"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -30,6 +31,7 @@ func New() Service {
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(database).SetServerAPIOptions(serverAPI))
+	CreateTodoCollectionIfDoesntExist(client)
 
 	if err != nil {
 		log.Fatal(err)
@@ -52,4 +54,33 @@ func (s *service) Health() map[string]string {
 	return map[string]string{
 		"message": "It's healthy",
 	}
+}
+
+func CreateTodoCollectionIfDoesntExist(db *mongo.Client) {
+	databases, err := db.ListDatabaseNames(context.Background(), bson.M{})
+	dbName := "todo-app"
+	containsDbName := false
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, db := range databases {
+		if db == dbName {
+			containsDbName = true
+		}
+	}
+
+	if !containsDbName {
+		collection := db.Database(dbName).Collection("users_log")
+		_, err := collection.InsertOne(context.Background(), bson.M{
+			"system": "createdDb",
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("todo-app Db created with todos collection ")
+		return
+	}
+	log.Println("Db todo-app already exists, no need for creation")
+
 }
